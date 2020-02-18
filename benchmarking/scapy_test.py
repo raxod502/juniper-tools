56e0e0d4f75ea8bf703ae46d62a913970e3f848c
Miles President <mcprez10@gmail.com>
2020-02-17 17:33:23 -0800

    Intermediate router working.

--- benchmarking/scapy_test.py
+++ benchmarking/scapy_test.py
@@ -1,10 +1,14 @@
-from scapy.all import IPv6, IPv6ExtHdrRouting, UDP, Raw, send
+from scapy.all import Ether, IPv6, IPv6ExtHdrRouting, UDP, Raw, sendp, ls
+from threading import Timer, Lock
 from struct import pack
-
-import argparse
+from time import time
+from argparse import ArgumentParser
 
 
 def makePacket(dstip, rthdr):
+    # Receiver VM MAC address. Don't know why Scapy can't figure this out on its own.
+    eth = Ether(dst="08:00:27:a1:ca:9a")
+
     iphdr = IPv6()
     iphdr.dst = dstip
     iphdr.nh = 43       # Routing Header
@@ -12,10 +16,10 @@ def makePacket(dstip, rthdr):
     udphdr = UDP()
     udphdr.sport = 11111
     udphdr.dport = 3000
-    
+
     payload = "$"
 
-    return iphdr / rthdr / udphdr / payload
+    return eth / iphdr / rthdr / udphdr / payload
 
 
 def makeRH0(dstip, addrs):
@@ -37,7 +41,7 @@ def makeCRH16(dstip, sids):
     routingType = 5
     segLeft = 1
     crh = pack(fmt, nextHeader, extLen, routingType, segLeft, *sids)
-    
+
     return makePacket(dstip, crh)
 
 
@@ -48,23 +52,33 @@ def makeCRH32(dstip, sids):
     routingType = 6
     segLeft = 1
     crh = pack(fmt, nextHeader, extLen, routingType, segLeft, *sids)
-    
+
     return makePacket(dstip, crh)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Send RH0 and CRH packets.")
-    parser.add_argument("type", choices=["rh0", "crh16", "crh32"])
-    parser.add_argument("-d", "--dstip", type=str, default="2001:4860:4860::8888",  help="Destination IPv6 address")
-    parser.add_argument("-n", "--numDevices", type=int, default="5", help="The number of IP addresses in the routing extension header")
-    parser.add_argument("-t", "--numTimes", type=int, default="1", help="The number of times to send the packet (-1 to send indefinitely).")
+    receiverVmIp = "fde4:8dba:82e1::c4"
+    routerVmIp = "fde4:8dba:82e0::c5"
+
+    parser = ArgumentParser("Send RH0 and CRH packets.")
+    parser.add_argument("type", choices=["rh0", "crh16", "crh32"],
+        help="Type of routing extension header")
+    parser.add_argument("-d", "--dstip", type=str, default=receiverVmIp,
+        help="Destination IPv6 address")
+    parser.add_argument("-s", "--size", type=int, default=5,
+        help="The number of IP addresses in the routing extension header")
+    parser.add_argument("-c", "--count", type=int, default=1,
+        help="The number of packets to send.")
+    parser.add_argument("-i", "--interval", type=int, default=0,
+        help="The time (in seconds) between sending two packets.")
     args = parser.parse_args()
 
     if args.type == "rh0":
-        # Random addresses
+        # We want to go through the router first, then
+        # to our receiver, and then we don't care.
         addrs = [
-            "f223:7036:7677:67e5:cca2:abe1:eec3:5718",
-            "72d8:7613:5172:860f:6d70:5260:ba7b:2053",
+            routerVmIp,
+            receiverVmIp,
             "b03b:c9d2:bd5d:923e:5adf:9675:e903:27ea",
             "5d45:828f:f53b:e43c:ef68:6991:a9ae:5a9b",
             "6374:f8d4:e316:fc7c:279b:5884:fd9e:ddf4",
@@ -76,18 +90,24 @@ if __name__ == "__main__":
             "c85d:1618:799:8c6:41c2:2dc6:83e9:175",
             "4bd7:4270:d60e:a973:5c92:b4ec:fbb3:9562"
         ]
-        pkt = makeRH0(args.dstip, addrs[:args.numDevices])
+        pkt = makeRH0(args.dstip, addrs[:args.size])
     else:
-        # Random SIDs
+        # Random SIDs. TODO: These will need to be set up correctly.
         sids = [1,2,3,4,5,6,7,8,9,10,11,12]
         if args.type == "crh16":
-            pkt = makeCRH16(args.dstip, sids[:args.numDevices])
+            pkt = makeCRH16(args.dstip, sids[:args.size])
         else:
-            pkt = makeCRH32(args.dstip, sids[:args.numDevices])
+            pkt = makeCRH32(args.dstip, sids[:args.size])
 
-    if args.numTimes == -1:
-        while True:
-            send(pkt)
-    else:
-        for i in range(args.numTimes):
-            send(pkt)
+    print(
+        f"Sending {args.count} {args.type} packet(s) to {args.dstip} with "
+        f"{args.size} device(s) and an interval of {args.interval}.")
+    print()
+    print("---------------\nPACKET FIELDS\n---------------")
+    print(ls(pkt))
+
+    start = time()
+    sendp(pkt, count=args.count, inter=args.interval, iface="enp0s8")
+    stop = time()
+
+    print(f"Took {stop - start} seconds (wall clock).")
