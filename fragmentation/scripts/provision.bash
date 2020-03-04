4fc6c464a01d54a0f6e77a47981253c40b2e0a73
Radon Rosborough <radon.neon@gmail.com>
2020-03-04 09:18:50 -0800

    Get rid of Juniper shell profile

--- fragmentation/scripts/provision.bash
+++ fragmentation/scripts/provision.bash
@@ -37,14 +37,3 @@ apt-get update
 apt-get install -y $(grep -v "^#" <<< "$packages")
 # No need to remove /var/lib/apt/lists as we're not building a Docker
 # image.
-
-# Try to be idempotent.
-if ! grep "source /vagrant/scripts/profile.bash" /home/vagrant/.bashrc &>/dev/null; then
-    cat <<"EOF" >> /home/vagrant/.bashrc
-if [ -f /vagrant/scripts/profile.bash ]; then
-    source /vagrant/scripts/profile.bash
-else
-    echo "warning: Juniper shell profile not available" >&2
-fi
-EOF
-fi
