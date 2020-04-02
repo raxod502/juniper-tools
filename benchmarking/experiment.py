972621f12c2b8a8ad391e2f2afa2b230d2aaa257
Radon Rosborough <radon.neon@gmail.com>
2020-04-02 13:06:35 -0600

    Reformat experiment.py, make executable

--- benchmarking/experiment.py
+++ benchmarking/experiment.py
@@ -1,10 +1,8 @@
+#!/usr/bin/env python3
+
 import subprocess
 
-startIntervals = {
-    "rh0": 1.0,
-    "srh": 1.1,
-    "crh16": 0.2
-}
+startIntervals = {"rh0": 1.0, "srh": 1.1, "crh16": 0.2}
 
 for hdr in ["rh0", "crh16", "srh"]:
     startI = startIntervals[hdr]
@@ -23,60 +21,61 @@ for hdr in ["rh0", "crh16", "srh"]:
                 str(startI),
                 "-n",
                 "2",
-                "-d"
-                "0.01",
+                "-d" "0.01",
                 "-s",
                 str(size),
                 "-f",
                 hdr + ".json",
-                "-v"
-            ))
+                "-v",
+            )
+        )
         # Empirically, the throughput seems to increase linearly for RH0, so
         # let's keep these tests from taking forever.
         if hdr == "rh0":
             startI -= 0.05
-                
+
 
 # Regular packets with rh0 machine
 subprocess.run(
-            (
-                "sudo",
-                "python3",
-                "throughput.py",
-                "reg",
-                "-p",
-                "10",
-                "-c",
-                "10",
-                "-i",
-                "0.1",
-                "-n",
-                "2",
-                "-d"
-                "0.01",
-                "-f",
-                "reg_rh0.json",
-                "-v"
-            ))
+    (
+        "sudo",
+        "python3",
+        "throughput.py",
+        "reg",
+        "-p",
+        "10",
+        "-c",
+        "10",
+        "-i",
+        "0.1",
+        "-n",
+        "2",
+        "-d",
+        "0.01",
+        "-f",
+        "reg_rh0.json",
+        "-v",
+    )
+)
 
 # Regular packets with crh machine
 subprocess.run(
-            (
-                "sudo",
-                "python3",
-                "throughput.py",
-                "reg",
-                "-p",
-                "10",
-                "-c",
-                "10",
-                "-i",
-                "0.1",
-                "-n",
-                "2",
-                "-d"
-                "0.01",
-                "-f",
-                "reg_crh.json",
-                "-v"
-            ))
+    (
+        "sudo",
+        "python3",
+        "throughput.py",
+        "reg",
+        "-p",
+        "10",
+        "-c",
+        "10",
+        "-i",
+        "0.1",
+        "-n",
+        "2",
+        "-d" "0.01",
+        "-f",
+        "reg_crh.json",
+        "-v",
+    )
+)
