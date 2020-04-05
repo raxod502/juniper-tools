430ee5cfb734bdf39d97277c966835016f5e5bf6
Radon Rosborough <radon.neon@gmail.com>
2020-04-05 16:06:22 -0600

    Quick fixes for type_avgThroughput.py

--- benchmarking/throughput_visual/type_avgThroughput.py
+++ benchmarking/throughput_visual/type_avgThroughput.py
@@ -1,13 +1,20 @@
-import pygal
+#!/usr/bin/env python3
+
+import argparse
 import json
 
-with open("./test1.json", "r") as throughputDataFile:
-    throughputData = throughputDataFile.read()
+import pygal
 
-results = json.loads(throughputData)
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+
+args = parser.parse_args()
+
+with open(args.file) as throughputDataFile:
+    results = json.load(throughputDataFile)
 
 typ_avg_thru = {}
-for result in results:
+for result in sorted(results, key=lambda r: r["numEntries"]):
     typ = result["type"]
     numEntries = result["numEntries"]
     typ_numEntries = typ + "(" + str(numEntries) + ")"
@@ -17,7 +24,7 @@ for result in results:
         typ_avg_thru[typ_numEntries] = throughput
 
 bar_graph = pygal.HorizontalBar()
-bar_graph.title = 'Header Type Average Throughput'
-for typ, thru in sorted(typ_avg_thru.items()):
+bar_graph.title = "Header Type Average Throughput"
+for typ, thru in typ_avg_thru.items():
     bar_graph.add(typ, thru)
-bar_graph.render_to_file('type_averageThru.svg')
+bar_graph.render_to_file("type_averageThru.svg")
