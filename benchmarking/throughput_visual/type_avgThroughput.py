7cdfe58a760bd0a721652c0778036bc534631d91
bnew10 <bradnew10@gmail.com>
2020-04-07 14:42:09 -0700

    fixed labeling of bar graph

--- benchmarking/throughput_visual/type_avgThroughput.py
+++ benchmarking/throughput_visual/type_avgThroughput.py
@@ -4,6 +4,7 @@ import argparse
 import json
 
 import pygal
+from pygal.style import BlueStyle
 
 parser = argparse.ArgumentParser()
 parser.add_argument("files", nargs="+")
@@ -25,8 +26,21 @@ for result in sorted(results, key=lambda r: (r["type"], r["numEntries"])):
     if typ_numEntries not in typ_avg_thru:
         typ_avg_thru[typ_numEntries] = throughput
 
-bar_graph = pygal.HorizontalBar()
-bar_graph.title = "Header Type Average Throughput"
+
+bar_graph = pygal.StackedBar(show_legend=False, style=BlueStyle)
+bar_graph.x_title = "Header Type"
+bar_graph.y_title = "Average Throughput"
+
+xLabels = []
+position = 0
+num_x_values = len(typ_avg_thru)
 for typ, thru in typ_avg_thru.items():
-    bar_graph.add(typ, thru)
+    xValues = [None for x in range(num_x_values)]
+    xValues[position] = thru
+    bar_graph.add(typ, xValues)
+    xLabels += [typ]
+    position += 1
+
+bar_graph.x_labels = xLabels
+
 bar_graph.render_to_file("type_averageThru.svg")
