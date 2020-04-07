2a66a3977473ff969cf0c2c3cdec2b79e9c26fd2
bnew10 <bradnew10@gmail.com>
2020-04-07 14:43:21 -0700

    deleted test_pygal.py

--- benchmarking/throughput_visual/test_pygal.py
+++
@@ -1,5 +0,0 @@
-import pygal  # First import pygal
-
-bar_chart = pygal.HorizontalBar()  # Then create a bar graph object
-bar_chart.add("Bradley", [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])  # Add some values
-bar_chart.render_to_file("bar_chart.svg")  # Save the svg to a file
