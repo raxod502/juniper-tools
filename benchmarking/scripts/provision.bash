452cbd7dd531340089ab371fcadd66c33b1220ff
Radon Rosborough <radon.neon@gmail.com>
2020-04-07 14:54:27 -0600

    Stick poetry in the sender VM
    Just in case.

--- benchmarking/scripts/provision.bash
+++ benchmarking/scripts/provision.bash
@@ -26,8 +26,9 @@ ntp
 # inspect packet traffic, for development & testing
 wireshark
 
-# scapy
+# scapy, poetry
 python3-pip
+python3-venv
 
 # look at our data files
 jq
@@ -38,5 +39,13 @@ apt-get update
 apt-get install -y $(grep -v "^#" <<< "$packages")
 # No need to remove /var/lib/apt/lists as we're not building a Docker
 # image.
+
+# Latest version of Poetry doesn't work on Ubuntu; workaround from
+# <https://bugs.launchpad.net/usd-importer/+bug/1794041/comments/10>.
+pip3 --disable-pip-version-check install -U keyrings.alt
+
+# Use Poetry to install project dependencies. Use the most recent
+# version because it just fixes *so* many bugs compared to pre-1.0.
+pip3 --disable-pip-version-check install poetry==1.0.0
 
 pip3 install scapy==2.4.3
