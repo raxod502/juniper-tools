24024ce9b8f00433613db790b052774e1ce260b4
bnew10 <bradnew10@gmail.com>
2020-04-10 12:22:32 -0700

    finishing actual_attempted

--- benchmarking/throughput_visual/actual_attempted.py
+++ benchmarking/throughput_visual/actual_attempted.py
@@ -1,16 +1,23 @@
-import pygal
+#!/usr/bin/env python3
+
+import argparse
 import json
+import os
 
-with open("./rh0_5.json", "r") as throughputDataFile:
-    throughputData = throughputDataFile.read()
+import pygal
 
-results = json.loads(throughputData)
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
 
+args = parser.parse_args()
 
+results = []
+with open(args.file) as throughputDataFile:
+    results = json.load(throughputDataFile)
 
-
-line_chart = pygal.Line()
-line_chart.title = 'Attempted Throughput vs Actual Throughput'
-xyLabels = [(pair[0], pair[1]) for pair in results]
-line_chart.add('rh0(5)', x
-line_chart.render()
+line_chart = pygal.XY(show_legend=False)
+fileName = " (" + os.path.basename(args.file) + ")"
+line_chart.title = "Attempted Throughput vs Actual Throughput" + fileName
+xyCoords = [(pair[0], pair[1]) for pair in sorted(results, key=lambda r: r[0])]
+line_chart.add('', xyCoords)
+line_chart.render_to_file('actual_attempted.svg')
