549556d0b6310c5f440a60c84804d19cb6a6bef8
bnew10 <bradnew10@gmail.com>
2020-04-11 22:35:17 -0700

    changes to type_avgThroughput

--- benchmarking/throughput_visual/type_avgThroughput.py
+++ benchmarking/throughput_visual/type_avgThroughput.py
@@ -16,32 +16,38 @@ for fname in args.files:
     with open(fname) as throughputDataFile:
         results.extend(json.load(throughputDataFile))
 
-typ_avg_thru = {}
-for result in sorted(results, key=lambda r: (r["type"], r["numEntries"])):
+numEntries_typ_thru = {}
+xLabels = []
+for result in sorted(results, key=lambda r: (r["numEntries"], r["type"])):
     typ = result["type"]
     numEntries = result["numEntries"]
-    typ_numEntries = typ + "(" + str(numEntries) + ")"
     throughput = result["throughput"]
 
-    if typ_numEntries not in typ_avg_thru:
-        typ_avg_thru[typ_numEntries] = throughput
+    if numEntries not in numEntries_typ_thru:
+        numEntries_typ_thru[numEntries] = {}
+
+    if typ not in numEntries_typ_thru[numEntries]:
+       numEntries_typ_thru[numEntries][typ] = throughput
+       if typ not in xLabels:
+           xLabels += [typ]
+    else:
+        typ_num = typ + "(" + numEntries + ")"
+        errorMsg = "duplicate data for {}, unsure of which to use."
+        raise Exception(erroMsg.format(typ_num))
 
 
-bar_graph = pygal.StackedBar(x_label_rotation=-45, show_legend=False, style=BlueStyle)
+bar_graph = pygal.Bar(show_legend=False, width=1750, print_labels=True, style=BlueStyle)
+
 bar_graph.title = "Average Throughput Per Header Type"
-bar_graph.x_title = "Header Type"
-bar_graph.y_title = "Average Throughput"
-
-xLabels = []
-position = 0
-num_x_values = len(typ_avg_thru)
-for typ, thru in typ_avg_thru.items():
-    xValues = [None for x in range(num_x_values)]
-    xValues[position] = thru
-    bar_graph.add(typ, xValues)
-    xLabels += [typ]
-    position += 1
-
+bar_graph.x_title = "Header Type (Number of Entries)"
+bar_graph.y_title = "Average Throughput (Packets)"
 bar_graph.x_labels = xLabels
+
+xAxisLen = len(xLabels)
+for numEntries, typList in numEntries_typ_thru.items():
+    xValues = [None for x in range(xAxisLen)]
+    for typ, throughput in typList.items():
+        xValues[xLabels.index(typ)] = {'value': throughput, 'label': str(numEntries)}
+    bar_graph.add(str(numEntries), xValues)
 
 bar_graph.render_to_file("type_averageThru.svg")
