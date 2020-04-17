e8477feb9d4b55758fa01dcbbb66f4ca40925946
bnew10 <bradnew10@gmail.com>
2020-04-16 20:46:18 -0700

    removed extra bar graph script and extraneous lines of code

--- benchmarking/throughput_visual/type_avgThroughput.py
+++ benchmarking/throughput_visual/type_avgThroughput.py
@@ -20,45 +20,42 @@ for fname in args.files:
     with open(fname) as throughputDataFile:
         results.extend(json.load(throughputDataFile))
 
-numEntries_typ_thru = {}
+typ_numEntries_thru = {}
+typeList = []
 xLabels = []
 for result in sorted(results, key=lambda r: (r["numEntries"], r["type"])):
     typ = result["type"]
     numEntries = result["numEntries"]
     throughput = result["throughput"]
 
-    if numEntries not in numEntries_typ_thru:
-        numEntries_typ_thru[numEntries] = {}
+    if typ not in typ_numEntries_thru:
+        typ_numEntries_thru[typ] = {}
+        typeList += [typ]
 
-    if typ not in numEntries_typ_thru[numEntries]:
-       numEntries_typ_thru[numEntries][typ] = throughput
-       if typ not in xLabels:
-           xLabels += [typ]
+    if numEntries not in typ_numEntries_thru[typ]:
+       typ_numEntries_thru[typ][numEntries] = throughput
+       if numEntries not in xLabels:
+           xLabels += [numEntries]
     else:
         typ_num = typ + "(" + numEntries + ")"
         errorMsg = "duplicate data for {}, unsure of which to use."
         raise Exception(erroMsg.format(typ_num))
 
 
-custom_style = BlueStyle()
-labelSize = 17
-custom_style.label_font_size = labelSize
-custom_style.major_label_font_size = labelSize
-custom_style.title_font_size = 23
-custom_style.value_label_font_size = 11
-
-bar_graph = pygal.Bar(show_legend=False, width=1750, print_labels=True, style=custom_style)
+# configure bar graph
+bar_graph = pygal.Bar(style=BlueStyle)
 bar_graph.title = "Average Throughput Per Header Type"
-bar_graph.x_title = "Header Type (Number of Entries)"
+bar_graph.x_title = "Number of Entries (addresses)"
 bar_graph.y_title = "Average Throughput (packets)"
-bar_graph.x_labels = xLabels
+bar_graph.x_labels = sorted(xLabels)
 
+# add x values for each header type
 xAxisLen = len(xLabels)
-for numEntries, typList in numEntries_typ_thru.items():
+for typ, numEntriesList in typ_numEntries_thru.items():
     xValues = [None for x in range(xAxisLen)]
-    for typ, throughput in typList.items():
-        xValues[xLabels.index(typ)] = {'value': throughput, 'label': str(numEntries)}
-    bar_graph.add(str(numEntries), xValues)
+    for numEntries, throughput in numEntriesList.items():
+        xValues[numEntries-1] = throughput
+    bar_graph.add(typ, xValues)
 
 if args.filetype == "png":
     bar_graph.render_to_png("type_averageThru.png")
